d81b4714b5b2b9f994cb4a66b73c825bcd134a1b
asha <ashahosamani412@gmail.com>
2020-01-06 15:07:34 -0500

    updating with some report format changes

--- src/org/automation/base/DriverInstance.java
+++ src/org/automation/base/DriverInstance.java
@@ -8,7 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -17,14 +21,17 @@ public class DriverInstance {
     
 	public WebDriver driver;
 	
-	public ExtentReports extentReports;
-	public ExtentTest extentTest;
+	public static ExtentReports extentReports;
+	public static ExtentTest extentTest;
+	
+	@BeforeTest
+	public void initiateReport() {
+		extentReports=new ExtentReports("./Reports/report.html", true);
+		extentReports.addSystemInfo("Author name", "Facebook");
+	}
 	
 	@BeforeMethod
-	public void InitiateDriverInstance() throws IOException {
-		extentReports=new ExtentReports("./Reports/report.html", false);
-		extentReports.addSystemInfo("Author name", "Facebook");
-		
+	public void InitiateDriverInstance() throws IOException {		
 		if(Utility.fetchProperty("browserName").toString().equalsIgnoreCase("chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
@@ -56,10 +63,16 @@ public class DriverInstance {
 	}
 	
 	@AfterMethod
+	public void closeWedDriverInstance() {
+		driver.close();
+		driver.quit();
+	}
+	
+	@AfterTest
 	public void CloseDriverInstance() {
 		
 		extentReports.flush();
 
-		driver.close();
+		
 	}
 }
